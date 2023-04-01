0107c6fa34006e209440264fc2bc146e7e627932
nahlamohamed2000 <63589862+nahlamohamed2000@users.noreply.github.com>
2023-04-01 16:03:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const el=document.getElementById('root');
+const root=ReactDOM.createRoot(el);
+root.render(<App />);
+//components
+//book create for searching book names
+//booklist showing list of books
+//bookshow for showing each individual book
+//bookedit showing updated fields when edit button clicked
